bf70a6cb81db8f587e51a805ca13bd0cbbc76adc
L3on-w <94245792+L3on-w@users.noreply.github.com>
2022-01-09 21:33:40 -0500

    Add files via upload

---
+++ Movies.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace movie_list
+{
+    public class Movies
+    {
+        public List<string> Movie { get; set; }
+
+        public string title
+        {
+            set
+            {
+                _title = value;
+            }
+            get
+            {
+                return _title;
+            }
+        }
+        private string _title;
+
+        public string category
+        {
+            set
+            {
+                _category = value;
+            }
+            get
+            {
+                return _category;
+            }
+        }
+        private string _category;
+
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace movie
+{
+    internal class program {
+
+
+        public List<Title> Movie = new List<Title>();
+        Movie.Add(Star Wars, scifi);
+        Movie.Add(Title(){"Star Trek" "scifi"});
+        Movie.Add(Title("Aliens" "scifi"));
+        Movie.Add(Title("Wreck it Ralph" "animated"));
+        Movie.Add(Title("Toy Story" "animated"));
+        Movie.Add(Title("Spider-Man: Into the Spider-Verse" "animated"));
+        Movie.Add(Title("Saw" "horror"));
+        Movie.Add(Title("Get Out" "horror"));
+        Movie.Add(Title("Knives out" "drama"));
+        Movie.Add(Title("The Social Network" "drama"));
+
+        foreach (Title title in Movie)
+        {
+            Console.WriteLine(title);
+        }
+
+
+       
+    }
+}
